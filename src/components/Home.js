abcc5b74d8033e64651d8885e51d69ac2a03c8d9
Uyen <contact@uyen.io>
2018-06-13 00:38:07 +0700

    Feature: Adding tabs to Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,32 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Question from './Question'
 import QuestionDetails from './QuestionDetails'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                <ul className='home-list'> 
-                    {this.props.questionIds.map((id) => (
-                        <li key={id}>
-                            <Question id={id}/>
-                        </li>
-                    ))}
-                </ul>
-            </div>  
+            <Tabs>
+            <TabList>
+              <Tab>Unanswered</Tab>
+              <Tab>Answered</Tab>
+            </TabList>
+         
+            <TabPanel>
+                <div>
+                    <ul className='home-list'> 
+                        {this.props.questionIds.map((id) => (
+                            <li key={id}>
+                                <Question id={id}/>
+                            </li>
+                        ))}
+                    </ul>
+                </div> 
+            </TabPanel>
+            <TabPanel>
+                Content 2
+            </TabPanel>
+          </Tabs> 
         )
     }
 }
